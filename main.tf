3714cd15a3bb2a93d50882e516339a923326d0cb
ken.sun <ken.sun@ssc-spc.gc.ca>
2022-10-24 14:47:34 -0400

    test

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_resource_group_template_deployment" "linux" {
                         }
                     },
                     "scheduleInfo": {
-                        "frequency": "${var.frequency}",
+                        "frequency": ${jsonencode(var.frequency)},
                         "startTime": "${local.update_date}T${local.update_time}:00-00:00",
                         "timeZone": "${var.timeZone}",
                         "interval": ${var.interval},
@@ -115,7 +115,7 @@ resource "azurerm_resource_group_template_deployment" "windows" {
                         }
                     },
                     "scheduleInfo": {
-                        "frequency": "${var.frequency}",
+                        "frequency": ${jsonencode(var.frequency)},
                         "startTime": "${local.update_date}T${local.update_time}:00-00:00",
                         "timeZone": "${var.timeZone}",
                         "interval": ${var.interval},
